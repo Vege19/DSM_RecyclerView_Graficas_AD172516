3ad58792048c85903a66e971c18c1783c43db570
Vege19 <josue.ardon113@gmail.com>
2019-03-05 07:43:18 -0600

    PurposeDetailsActivity creado

---
+++ app/src/main/java/com/example/recyclerviewgraphview/PurposeDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.recyclerviewgraphview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PurposeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purpose_details);
+    }
+}
